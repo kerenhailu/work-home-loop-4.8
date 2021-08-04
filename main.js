847e330e315d7715759e1fdfb0d93506dc82ba3f
kerenhailu <keren4509@gmail.com>
2021-08-05 00:30:09 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,112 @@
+// ---------- שאלה ראשונ ה -------------
+// var num1, num2
+// num1 = prompt("number one");
+// num2 = prompt("number two");
+
+// console.log(num1, num2);
+
+// if (num2+num1<=13) {
+//     console.log("bar-mitzva")
+// } 
+// else
+// if (num2+num1==12) {
+//     console.log("bat-mitzva")}
+// else {
+//     console.log("kid")
+// }
+// ----------- שאלה שניה ------------------
+// var i = 0;
+// while (i < 100){
+//     console.log("yes");
+//     i++
+// }
+// var i = 0;
+// var a =Number(prompt("choose a number")) ;
+// while(i<a){
+//     console.log(`no`);
+//     i++
+// }
+// --------------שאלה שלישית--------------------
+// for (i=0;i<1000;i++) {
+//     console.log("javascript is not JAVA");
+// }
+// --------------שאלה רביעית--------------------
+// var name = prompt("your name");
+// var number =Number(prompt("choose a number")) ;
+// for (i=0;i<number;i++){
+//     console.log(name);
+// }
+// ------------שאלה חמישית --------------------
+// var Name , Number , Email ;
+// Name = prompt("what your name ?");
+// Number = prompt("choose a number??");
+// Email = prompt("email ?");
+// for (i=0;i<Number;i++)
+// if (Number>18){
+//     console.log(`${Name} , ${Email}`);
+// }
+// --------------- שאלה שישית -------------------
+// nUmBeR =Number(prompt("numberrrr ?")) ;
+// nAmE = prompt("nameee ?")
+// for (i=0;i<nUmBeR;i++){
+//     if (nAmE=="keren"){
+//         console.log(`hi boss , ${nAmE}`);
+//     }
+//     else{
+//         console.log("no entry")
+//     }
+// }
+// --------------- שאלה שביעית -------------------
+// var numONE , numTWO ;
+// numONE = Number(prompt("number one ?"));
+// numTWO = Number(prompt("number two ?"));
+// for (i=0;i<numONE;i++){
+//     for(j=0;j<numTWO;j++){
+//         // console.log(`i , ${i} , j , ${j}`);
+//         console.log(i*j);
+//     }
+// }
+// -----------------שאלה שמינית ------------------
+// var numBERS = [4,6,2,1,5]
+// console.log(numBERS);
+// for(i=0;i<numBERS.length;i++){
+//     console.log(`${i} , ${numBERS[i]}` );
+// }
+// ------------ שאלה תשיעית ------------------------
+// var nambers = [51, 72, 83, 24, 35, 46, 67, 12];
+// for (i = 0; i < nambers.length; i++){
+//     if(nambers[i]%2 == 0){
+//     console.log(nambers[i]);
+// }
+// }
+// ---------------- שאלה עשירית ----------------------
+// var stuff =[]
+// for(i=0;i<5;i++){
+//     stuff[i]=prompt("choose names");
+// }
+// console.log(stuff);
+// ----------- שאלה אחד עשרה -------------
+var numberSeven = []
+for (i = 0; i < 7; i++) {
+    numberSeven[i] = prompt("seven number")
+    if (numberSeven[i] % 2 != 0) {
+        console.log(numberSeven[i])
+    }
+}console.log(numberSeven)
+// }--------שאלה שתיים עשרה-------------------
+var stuff = ["keren", "hailu", 22, 67, "red", 12];
+for (i = 0; i < stuff.length; i++) {
+    if (stuff[i] * 0 != 0) {
+        stuff[i] = "not number";
+    }
+}
+console.log(stuff);
+// --------------- שלוש עשרה -------------------------
+var numberTEN = []
+for (i = 0; i < 10; i++) {
+    numberTEN[i] = prompt("ten number")
+    if (numberTEN[i] % 2 == 0) {
+        console.log(numberTEN[i])
+    }
+}
+// ------------ ארבע עשרה ------------------------------------
